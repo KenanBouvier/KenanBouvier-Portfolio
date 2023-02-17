77e346c675072d4fc488230e3d76d1ad3852339b
Kenan Bouvier <kenanbouvier@Kenans-MacBook-Pro.local>
2023-02-17 20:24:52 +0000

    reponsive styles

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBar(){
   return (
     <div className='navDiv'>
       <nav className="navMenu">
-        <a href="/">Software Development</a>
+        <a href="/">Projects</a>
         <a href="cyber">Cyber Security</a>
         <a href="about">About me</a>
         <div className="dot"></div>
